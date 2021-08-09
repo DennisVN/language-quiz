d5f40b67e147f6ef8df56eddf7927a3d6a8545c6
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-08-09 10:38:58 +0200

    Add whatIsHappening to see what is happening. Underrated step ;)

--- Starter-pack/index.php
+++ Starter-pack/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // Require the correct variable type to be used (no auto-converting)
 declare(strict_types = 1);
 
@@ -7,6 +8,22 @@ declare(strict_types = 1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+// Debug tools :
+function whatIsHappening()
+{
+    echo '<pre>';
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+    echo '</pre>';
+}
+whatIsHappening();
 
 // Load your classes
 require_once 'classes/Data.php';
@@ -17,8 +34,7 @@ require_once 'classes/Word.php';
 // Start the game
 $game = new LanguageGame();
 $game->run();
-// TODO : Create field that displays random word
-echo "ik werk";
-// TODO : Create input Field for user to type translation
-// TODO : Add Submit Field (no button right ? )
+
+
+
 require 'view.php';
