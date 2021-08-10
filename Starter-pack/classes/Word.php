b71e8564b6d9507e979b4d448326a8b6ce5d1f09
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-08-10 14:19:03 +0200

    WIP verification. If playerguess == the correct answer -> return true

--- Starter-pack/classes/Word.php
+++ Starter-pack/classes/Word.php
@@ -14,7 +14,11 @@ class Word
     public function verify(string $playerGuess)
     {   
         // TODO: use this function to verify if the provided answer by the user matches the correct one
-        echo($playerGuess);
+        if ($playerGuess == $this->answer){
+            return true;
+        } else {
+            return false;
+        }
         // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
         // Bonus (hard): can you allow answers with small typo's (max one character different)?
     }
