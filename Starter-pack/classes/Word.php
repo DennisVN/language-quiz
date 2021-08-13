8d4522c001a4ca4cc39fd3ef945b97823ee60ec8
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-13 10:53:23 +0200

    WIP re-implement verify function , use this in arrow declaration later ;)

--- Starter-pack/classes/Word.php
+++ Starter-pack/classes/Word.php
@@ -13,13 +13,13 @@ class Word
 
     public function verify(string $playerGuess)
     {   
-        echo($playerGuess);
+        //echo($playerGuess);
         // TODO: use this function to verify if the provided answer by the user matches the correct one
-        /*if ($playerGuess === $this->answer){
+        if ($playerGuess === $this->answer){
             return true;
         } else {
             return false;
-        }*/
+        }
         // Bonus: allow answers with different casing (example: both bread or Bread can be correct answers, even though technically it's a different string)
         // Bonus (hard): can you allow answers with small typo's (max one character different)?
     }
